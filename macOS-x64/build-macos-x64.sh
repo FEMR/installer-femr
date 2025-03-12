7d1615b8d11c5374ca3ac16385b5c9cdcd0eca42
realkaranvir <113927390+realkaranvir@users.noreply.github.com>
2025-03-11 21:10:08 -0700

    installer fixes

--- macOS-x64/build-macos-x64.sh
+++ macOS-x64/build-macos-x64.sh
@@ -147,8 +147,8 @@ copyBuildDirectory() {
     chmod -R 755 "${TARGET_DIRECTORY}"/darwinpkg/Library/${PRODUCT}/${VERSION}
     
     #Changes the script dir variable to the target path. This used to start the docker compose later.
-    sed -i -e 's#__SCRIPT_DIR__#'${TARGET_DIRECTORY}'/darwinpkg/Library/'${PRODUCT}'/'${VERSION}'#g' "${TARGET_DIRECTORY}/darwinpkg/Library/${PRODUCT}/${VERSION}/start-femr"
-    
+    sed -i -e 's#__SCRIPT_DIR__#''/var/'${PRODUCT}'#g' "${TARGET_DIRECTORY}/darwinpkg/Library/${PRODUCT}/${VERSION}/start-femr"
+
     rm -rf "${TARGET_DIRECTORY}/package"
     mkdir -p "${TARGET_DIRECTORY}/package"
     chmod -R 755 "${TARGET_DIRECTORY}/package"
