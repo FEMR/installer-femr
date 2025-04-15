92a4c30cb64ba9239c6106bd931720605cbcc408
realkaranvir <113927390+realkaranvir@users.noreply.github.com>
2025-04-15 10:02:17 -0700

    updated

--- release.py
+++ release.py
@@ -26,24 +26,24 @@ def main():
 
     print("Building Intel Mac installer.")
     # build the mac installer
-    subprocess.run(['./macOS-x64/build-macos-x64.sh', 'femr', args.version, 1], input=b'N', check=True)
+    subprocess.run(['./macOS-x64/build-macos-x64.sh', 'femr', args.version, "1"], input=b'N', check=True)
 
     print('Uploading Intel Mac installer to S3. This may take a while...')
 
     s3.upload_file(
-        f'./macOS-x64/target/pkg/femr-macos-installer-x64-{args.version}.pkg', 
+        f'./macOS-x64/target/pkg/femr-macos-installer-intel-{args.version}.pkg', 
         BUCKET_NAME, 
         f'macos/intel/{args.version}/femr-x64-{args.version}.pkg'
     )
 
     print("Building Apple Silicon Mac installer.")
     # build the mac installer
-    subprocess.run(['./macOS-x64/build-macos-x64.sh', 'femr', args.version, 2], input=b'N', check=True)
+    subprocess.run(['./macOS-x64/build-macos-x64.sh', 'femr', args.version, "2"], input=b'N', check=True)
 
     print('Uploading Arm Mac installer to S3. This may take a while...')
 
     s3.upload_file(
-        f'./macOS-x64/target/pkg/femr-macos-installer-x64-{args.version}.pkg', 
+        f'./macOS-x64/target/pkg/femr-macos-installer-arm-{args.version}.pkg', 
         BUCKET_NAME, 
         f'macos/arm/{args.version}/femr-x64-{args.version}.pkg'
     )
